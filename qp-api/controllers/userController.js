fc455254deee124d7a8528e01b848645f19b8bfd
nirojasm-u <nirojasm@unal.edu.co>
2024-12-18 10:53:59 -0500

    User Controller queries

--- qp-api/controllers/userController.js
+++ qp-api/controllers/userController.js
@@ -1,1 +1,74 @@
 const pool = require('../db');
+
+const getUsers = async (req, res) => {
+    try {
+        const users = await pool.query('SELECT * FROM users');
+        res.json(users.rows);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+const getUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user.rows);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+const createUser = async (req, res) => {
+    const { name, email, age, description } = req.body;
+    try {
+        const result = await pool.query(
+            'INSERT INTO users (name, email, age, description) VALUES ($1, $2, $3, $4) RETURNING *',
+            [name, email, age, description]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const updateUser = async (req, res) => {
+    const { id } = req.params;
+    const { name, email, age, description } = req.body;
+    try {
+        const result = await pool.query(
+            'UPDATE users SET name = $1, email = $2, age = $3, description = $4 WHERE id = $5 RETURNING *',
+            [name, email, age, description, id]
+        );
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const deleteUser = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+};
