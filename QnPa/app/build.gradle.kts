530973dc056491415f086251df66b1fd5484ca32
David Casallas <61563462+DavidCMeneses@users.noreply.github.com>
2025-02-15 09:47:23 -0500

    Added Resquests
    Se agregaron las request

--- QnPa/app/build.gradle.kts
+++ QnPa/app/build.gradle.kts
@@ -51,6 +51,9 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
 
